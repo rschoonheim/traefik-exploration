73d2a93e3d7157563ad4795a11d925becbd8ceb9
romano schoonheim <romano@romanoschoonheim.nl>
2024-07-19 15:14:12 +0200

    Generate root and intermediate CA.

--- scripts/tls.sh
+++ scripts/tls.sh
@@ -34,8 +34,59 @@ if [ ! -d .tls ]; then
   echo 1000 > .tls/serial
   echo 1000 > .tls/intermediate/serial
 
-  # Copy the openssl configuration files
+  # Copy configs
   #
   cp config/tls/ca.cnf .tls/openssl.cnf
   cp config/tls/intermediate.cnf .tls/intermediate/openssl.cnf
 fi
+
+# Generate root CA key and certificate.
+#
+openssl genrsa -out .tls/private/ca.key.pem 4096
+
+openssl req \
+  -config /app/.tls/openssl.cnf \
+  -key .tls/private/ca.key.pem -new -x509 \
+  -days 7300 \
+  -sha256 \
+  -extensions v3_ca \
+  -out .tls/certs/ca.cert.pem \
+  -subj "/C=NL/ST=Utrecht/L=Utrecht/O=Rschoonheim/CN=Example Root CA"
+
+# Permissions setup for the root CA key and certificate.
+#
+chmod 400 .tls/private/ca.key.pem
+chmod 444 .tls/certs/ca.cert.pem
+
+# Verify the root CA certificate.
+#
+openssl verify -CAfile .tls/certs/ca.cert.pem .tls/certs/ca.cert.pem
+
+# Generate intermediate CA key, signing request and certificate.
+#
+openssl genrsa -out .tls/intermediate/private/intermediate.key.pem 4096
+
+openssl req \
+  -config /app/.tls/intermediate/openssl.cnf -new -sha256 \
+  -key .tls/intermediate/private/intermediate.key.pem \
+  -out .tls/intermediate/csr/intermediate.csr.pem
+
+openssl ca \
+  -config /app/.tls/openssl.cnf \
+  -extensions v3_intermediate_ca \
+  -days 3650 -notext -md sha256 \
+  -in .tls/intermediate/csr/intermediate.csr.pem \
+  -out .tls/intermediate/certs/intermediate.cert.pem
+
+# Remove signing request.
+#
+rm .tls/intermediate/csr/intermediate.csr.pem
+
+# Permissions setup for the intermediate CA key and certificate.
+#
+chmod 400 .tls/intermediate/private/intermediate.key.pem
+chmod 444 .tls/intermediate/certs/intermediate.cert.pem
+
+# Verify the intermediate CA certificate.
+#
+openssl verify -CAfile .tls/certs/ca.cert.pem .tls/intermediate/certs/intermediate.cert.pem
